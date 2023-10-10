85f489039a70fa6b614b5da8a4be4f643a64a72e
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-10 11:45:01 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,23 @@
+//1
+function multiply(a,b){
+    console.log( a*b );
+}
+//2
+const myMultiply = function (a,b){
+    console.log( a*b );
+}
+//3
+const myMultiplyFunc = (a,b) =>  console.log( a*b );
+//4
+function remainderOfDivision1(a,b) {
+    console.log( a%b );
+}
+
+const remainderOfDivision2 = function (a,b) {
+    console.log( a%b );
+}
+
+const remainderOfDivision3 = (a,b) => console.log( a%b );
+
+
+
